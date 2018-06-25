dcee1fd2a1406e8a85bb4bc19c20356a986f4e62
Zoe Barth <zoe.barth@gmail.com>
2018-06-25 15:46:49 -0500

    cleaning up tabs and spaces

--- app/controllers/admin/behemoths_controller.rb
+++ app/controllers/admin/behemoths_controller.rb
@@ -1,52 +1,52 @@
 module Admin
-class BehemothsController < ApplicationController
+  class BehemothsController < ApplicationController
 
-	def index
-		@behemoths = Behemoth.all.order(:name)
-	end
+    def index
+      @behemoths = Behemoth.all.order(:name)
+    end
 
-	def new
-	@behemoth = Behemoth.new
-	end
+    def new
+    @behemoth = Behemoth.new
+    end
 
-	def create
-	  @behemoth= Behemoth.new(behemoth_params)
-  	respond_to do |format|
-  	if @behemoth.save
-  		format.html { redirect_to admin_behemoth_path(@behemoth), notice: "Behemoth was successfully created."}
-  	else
-  		format.html {render :new}
-  	end
-  	end
-	end
+    def create
+      @behemoth= Behemoth.new(behemoth_params)
+      respond_to do |format|
+      if @behemoth.save
+        format.html { redirect_to admin_behemoth_path(@behemoth), notice: "Behemoth was successfully created."}
+      else
+        format.html {render :new}
+      end
+      end
+    end
 
-	def show 
-	set_behemoth
-	end
+    def show 
+    set_behemoth
+    end
 
-	def update
-	set_behemoth
-	 	respond_to do |format|
-  	if @behemoth.update(behemoth_params)
-  		format.html { redirect_to admin_behemoth_path(@behemoth), notice: "Behemoth was successfully updated."}
-  	else
-  		format.html {render :edit}
-  	end
-  	end
-	end
+    def update
+    set_behemoth
+      respond_to do |format|
+      if @behemoth.update(behemoth_params)
+        format.html { redirect_to admin_behemoth_path(@behemoth), notice: "Behemoth was successfully updated."}
+      else
+        format.html {render :edit}
+      end
+      end
+    end
 
-	def edit
-	set_behemoth
-	end
+    def edit
+    set_behemoth
+    end
 
-	private 
-	def set_behemoth
-	@behemoth = Behemoth.find(params[:id])
-	end
+    private 
+    def set_behemoth
+    @behemoth = Behemoth.find(params[:id])
+    end
 
-	def behemoth_params
-		params.fetch(:behemoth, {}).permit(:name)
-	end
+    def behemoth_params
+      params.fetch(:behemoth, {}).permit(:name)
+    end
 
-end
+  end
 end
