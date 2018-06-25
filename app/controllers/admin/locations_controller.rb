4065dccae669f8090f2bd07515180fbbc36d6720
Syed Abutalib <syedthegreatabutalib@gmail.com>
2018-06-25 16:59:01 -0500

    Third's time a charm

---
+++ app/controllers/admin/locations_controller.rb
@@ -0,0 +1,45 @@
+module Admin
+  class LocationsController < ApplicationController
+    def index
+      @locations = Location.all
+    end
+
+    def show
+      @location = Location.find(params[:id])
+    end
+
+    def new
+    end
+
+    def edit
+      @element = Location.find(params[:id])
+    end
+
+    def destroy
+      @location = Location.find(params[:id])
+      @location.destroy
+
+      redirect_to admin_locations_path
+    end
+
+    def update
+      @location = Location.find(params[:id]) 
+          if @location.update(locations_params)
+            redirect_to admin_locations_path
+          end
+    end
+
+    def create
+      @location = Location.new(locations_params)
+      @location.save
+      redirect_to admin_locations_path
+    end
+
+    private
+
+    def locations_params
+      params.require(:location).permit(:name)
+    end
+    
+  end
+end
