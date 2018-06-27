d82832cc14757d854f0d327482973139934baae8
Syed Abutalib <syedthegreatabutalib@gmail.com>
2018-06-27 17:49:11 -0500

    haha a merge

--- app/models/element.rb
+++ app/models/element.rb
@@ -3,4 +3,6 @@ class Element < ApplicationRecord
   has_many :elemental_damages
   has_many :weakened_behemoths, through: :elemental_weaknesses, class_name: 'Behemoth', source: :behemoth
   has_many :elemental_weaknesses
+
+  has_one_attached :avatar
 end
