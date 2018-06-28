17f053ab9b8fa1458a7166bd4f562673c17f366e
Syed Abutalib <syedthegreatabutalib@gmail.com>
2018-06-28 11:24:54 -0500

    removed the comment in the migration file

--- db/migrate/20180628152627_create_join_table_location_behemoth.rb
+++ db/migrate/20180628152627_create_join_table_location_behemoth.rb
@@ -2,7 +2,6 @@ class CreateJoinTableLocationBehemoth < ActiveRecord::Migration[5.2]
   def change
     create_join_table :locations, :behemoths do |t|
        t.index [:location_id, :behemoth_id]
-      # t.index [:behemoth_id, :location_id]
     end
   end
 end
