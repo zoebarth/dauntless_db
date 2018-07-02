86e48012fafe4d01457cb034bbd9afee45f13d43
Syed Abutalib <syedthegreatabutalib@gmail.com>
2018-07-02 11:03:10 -0500

    Removed a comment

--- db/migrate/20180629144445_add_column_to_locations.rb
+++ db/migrate/20180629144445_add_column_to_locations.rb
@@ -2,6 +2,5 @@ class AddColumnToLocations < ActiveRecord::Migration[5.2]
   def change
     add_reference :locations, :parent, foreign_key: true
 
-      #t.references :parent, index: true
   end
 end
