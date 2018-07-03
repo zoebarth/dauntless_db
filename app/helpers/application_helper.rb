912537177cb58a1b9b175a0efffd9bddb3a6d8ef
Syed Abutalib <syedthegreatabutalib@gmail.com>
2018-07-03 12:06:59 -0500

    Real highlighting

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def nav_link(title, path, controller_name = '')
+    klass = 'nav-item nav-link'
+    if controller_name == params[:controller]
+      klass += ' active'
+    end
+    link_to(title, path, class: klass)
+  end
 end
